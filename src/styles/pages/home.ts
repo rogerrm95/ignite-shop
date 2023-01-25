e68d43a0fa2aa0afa65a0bfed64718dc4189a4d5
rogerrm95 <rogerrm@live.com>
2023-01-24 22:41:13 -0300

    Visual Struture - creating Carrossel (Page: Home)

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled('main', {
     display: "flex",
-    gap: '3rem',
     width: '100%',
     maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))', // Responsividade //
     marginLeft: 'auto',
@@ -13,14 +12,13 @@ export const Product = styled('a', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
 
     borderRadius: 8,
-    padding: '0.25rem',
     cursor: 'pointer',
     position: 'relative',
 
     display: "flex",
     alignItems: 'center',
     justifyContent: 'center',
-    
+
     overflow: 'hidden',
 
     img: {
