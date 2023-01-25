f98419ee9b630d5b3b65975f1d6badac7f09bc5b
rogerrm95 <rogerrm@live.com>
2023-01-24 22:26:54 -0300

    Visual Struture - Home Structure (page)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,6 +12,13 @@ export const { css, keyframes, styled, globalCss, theme, config, createTheme, ge
 
             green500: '#00875f',
             green300: '#00b37e',
+        },
+
+        fontSizes: {
+            md: '1.125rem',
+            lg: '1.25rem',
+            xl: '1.5rem',
+            '2xl': '2rem'
         }
     }
 })
