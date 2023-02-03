5904be3a1e9c0bf0a5f2a001f6d14598822e8778
rogerrm95 <rogerrm@live.com>
2023-02-02 23:02:43 -0300

    Product & Checkout - Product Structure (Page)

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,33 @@
-export default function Product(){
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
+import { useRouter } from "next/router"
+
+export default function Product() {
+    const { query } = useRouter()
+
     return (
-        <>
-            <button>Enviar</button>
-        </>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+
+                <span>R$ 79.90</span>
+
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, 
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                It has survived not only five centuries, but also the leap into electronic typesetting, 
+                remaining essentially unchanged.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of 
+                Lorem Ipsum.
+                </p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,8 @@ import { globalCss } from '.'
 export const GlobalStyles = globalCss({
     '*': {
         margin: 0,
-        padding: 0
+        padding: 0,
+        boxSizing: 'border-box'
     },
 
     body: {
